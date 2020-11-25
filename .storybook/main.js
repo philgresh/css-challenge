58b91914e77a0ab403628b144c14ca98a1219551
Phil Gresham <philgresham@gmail.com>
2020-11-25 13:13:08 -0800

    Move example files to src/example/ and set up glob to read only non-examples for 'production'

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,27 @@
+const glob = require('glob');
+
+const EXAMPLE_FOLDER = 'example';
+const STORIES_GLOB = 'src/**/*.stories.@(js|jsx|ts|tsx)';
+const MDXES_GLOB = 'src/**/*.stories.mdx';
+const STORIES_MINUS_EXAMPLES_GLOB = `src/**/${EXAMPLE_FOLDER}/*.stories.@(js|jsx|ts|tsx)`;
+const MDXES_MINUS_EXAMPLES_GLOB = `src/**/${EXAMPLE_FOLDER}/*.stories.mdx'`;
+
+// options is optional
+const storiesFiles = () => {
+  const options = {};
+  if (process.env.STORYBOOK_ENV === 'production')
+    options.ignore = [STORIES_MINUS_EXAMPLES_GLOB, MDXES_MINUS_EXAMPLES_GLOB];
+
+  // Find stories
+  const stories = glob.sync(STORIES_GLOB, options).map((file) => `../${file}`);
+  const mdxes = [];
+  // const mdxes = glob.sync(MDXES_GLOB, options).map((file) => `../${file}`);
+
+  return [...stories, ...mdxes];
+};
+
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: async (list) => [...list, ...storiesFiles()],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
