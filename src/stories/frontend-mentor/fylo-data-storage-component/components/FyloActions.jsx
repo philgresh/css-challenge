678871ec2d176abe5817bcac448edf5deec2ac55
Phil Gresham <philgresham@gmail.com>
2020-12-03 17:06:54 -0800

    Feel good about FyloActions

--- src/stories/frontend-mentor/fylo-data-storage-component/components/FyloActions.jsx
+++ src/stories/frontend-mentor/fylo-data-storage-component/components/FyloActions.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { ReactComponent as Logo } from '../../../assets/logo.svg';
+import { ReactComponent as DocumentIcon } from '../../../assets/icon-document.svg';
+import { ReactComponent as FolderIcon } from '../../../assets/icon-folder.svg';
+import { ReactComponent as UploadIcon } from '../../../assets/icon-upload.svg';
 
-const FyloActions = (props) => {
-  return <div>Actions</div>;
+const Header = () => (
+  <div className="header">
+    <a href="https://fylo.com">
+      <Logo title="Fylo Logo" />
+    </a>
+  </div>
+);
+
+Header.propTypes = {};
+
+const Icon = ({ svgFile: SVGFile, title, link }) => (
+  <div className="icon">
+    <SVGFile title={title} />
+  </div>
+);
+
+Icon.propTypes = {
+  svgFile: PropTypes.element.isRequired,
+  title: PropTypes.string,
 };
+
+Icon.defaultProps = {
+  title: '',
+};
+
+const FyloActions = () => {
+  return (
+    <div className="actions">
+      <Header />
+      <div className="icons">
+        <Icon svgFile={DocumentIcon} title="New Document" />
+        <Icon svgFile={FolderIcon} title="New Folder" />
+        <Icon svgFile={UploadIcon} title="Upload Document" />
+      </div>
+    </div>
+  );
+};
+
+FyloActions.propTypes = {};
 
 export default FyloActions;
